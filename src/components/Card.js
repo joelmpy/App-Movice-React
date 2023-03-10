0e6679a586045c77a95031453b9801ed1f942d18
joel <joel_mpy@hotmail.com>
2023-03-10 15:28:02 +0100

    first

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,22 +5,17 @@ function Card(movie) {
 let img_movie = "https://image.tmdb.org/t/p/w500"
   return (
     <>
-      <div className="movie-card">
-        <img src={`${img_movie}${movie.info.poster_path}`} alt="" className="poster" />
-        <div className="movie-details">
-          <div className="box">
-            <h4 className="title-card">{movie.info.title ? movie.info.title : ""}</h4>
-            <p className="raiting">{movie.info.vote_average ? movie.info.vote_average : ""}</p>
+      <Link to={`/Movie/${movie.info.id}`} className="bn.62">
+        <div className="movie-card">
+          <img src={`${img_movie}${movie.info.poster_path}`} alt="" className="poster" />
+          <div className="movie-details">
+            <div className="box">
+              <h4 className="title-card">{movie.info.title ? movie.info.title : ""}</h4>
+              <p className="raiting">{movie.info.vote_average ? movie.info.vote_average : ""}</p>
+            </div>
           </div>
         </div>
-
-        <div className="link-overview">
-          <Link to={`/Movie/${movie.info.id}`} className="bn.62">
-          View more
-          </Link>
-        </div>
-
-      </div>
+      </Link>
     </>
   );
 }
--- src/components/ResultMain.js
+++ src/components/ResultMain.js
@@ -3,9 +3,9 @@ import React from 'react'
 function ResultMain(allmovies) {
     const image_api = "https://image.tmdb.org/t/p/w500"
   return (
-    <div>
+    <div className='main-card'>
         <img src={`${image_api}${allmovies.movies.poster_path}`} alt="" className="poster"/>
-       <p>{allmovies.movies.original_title}</p>
+       <p>{allmovies.movies.original_title ? allmovies.movies.original_title : ""}</p>
     </div>
   )
 }
