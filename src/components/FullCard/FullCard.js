ba100e80591e97a159713a529c6d4baf2367b28a
joel <joel_mpy@hotmail.com>
2023-03-10 16:49:41 +0100

    first

--- src/components/FullCard/FullCard.js
+++ src/components/FullCard/FullCard.js
@@ -16,20 +16,25 @@ function FullCard() {
       `https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
     )
       .then((res) => res.json())
-      .then((data) => setMovie(data));
+      .then((data) => 
+      setMovie(data));
   };
 
   return (
     <div className="movie">
+
         <div className="movie__intro">
             <img className="movie__backdrop" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} />
         </div>
+
+
         <div className="movie__detail">
             <div className="movie__detailLeft">
                 <div className="movie__posterBox">
                     <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
                 </div>
             </div>
+
             <div className="movie__detailRight">
                 <div className="movie__detailRightTop">
                     <div className="movie__name">{currentMovieDetail ? currentMovieDetail.original_title : ""}</div>
@@ -55,10 +60,12 @@ function FullCard() {
                 <div className="movie__detailRightBottom">
                     <div className="synopsisText">Synopsis</div>
                     <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
-                </div>
-                
+                </div> 
             </div>
         </div>
+
+
+
         <div className="movie__links">
             <div className="movie__heading">Useful Links</div>
             {
@@ -68,8 +75,13 @@ function FullCard() {
                 currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
             }
         </div>
-        <div className="movie__heading">Production companies</div>
+
+
+       
         <div className="movie__production">
+        <div className="movie__heading">
+           <h3>Production companies</h3> 
+        </div>
             {
                 currentMovieDetail && currentMovieDetail.production_companies && currentMovieDetail.production_companies.map(company => (
                     <>
